f969ff6f74ecca9ec8d7f45b2d1545504107c9c0
XuanThanhTb <xuanthanh180599@gmail.com>
2019-08-06 18:38:37 -0500

    fix removed

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('DSend Data', style: TextStyle(color: Colors.black, fontSize: 24),),
+          title: Text('Add Repair Removed', style: TextStyle(color: Colors.black, fontSize: 24),),
           backgroundColor: Colors.yellow,
         ),
         body: AddRepairRemoved(),
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget{
     );
   }
 }
+// ignore: must_be_immutable
 class AddRepairRemoved extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
@@ -25,10 +26,40 @@ class AddRepairRemoved extends StatefulWidget{
   }
 }
 class AddRepairRemovedState extends State<AddRepairRemoved>{
+  var list = List.generate(30, (item) => Data('$item'));
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+      alignment: Alignment.center,
+      child: ListView.builder(itemCount: list.length,
+         itemBuilder: (BuildContext context, int index){
+           final items = list[index].number;
+           return Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Dismissible(
+                key: Key(items),
+                direction: DismissDirection.horizontal,
+                onDismissed: (direction){
+                  setState(() {
+                    // items.removeAt(index);
+                    Scaffold.of(context).showSnackBar(SnackBar(backgroundColor: Colors.yellow, content: Text('$item dismissed'),));
+                  });
+                },
+              child: Text('$items', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 24),),
+              ),
+              Container(
+                color: Colors.red,
+              )
+             ],
+           );
+         },
+      ),
     );
   }
+}
+
+class Data{
+  String number;
+  Data(this.number);
 }
